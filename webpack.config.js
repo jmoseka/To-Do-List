34b26a55a6f3708591d8021d1f442c68fd044f8f
jmoseka <jamylam3@gmail.com>
2022-04-26 17:45:08 +0300

    install htmlwebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,4 +8,5 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
