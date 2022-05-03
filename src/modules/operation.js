7a788703717f6b32e0d143d65bc108643a0a169f
jmoseka <jamylam3@gmail.com>
2022-05-03 22:16:34 +0300

    Add new file module

---
+++ src/modules/operation.js
@@ -0,0 +1,17 @@
+import displayList from './taskFunctions.js';
+
+export default class TaskClass {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addTask(taskObjectClass) {
+    const listOfTask = JSON.parse(window.localStorage.getItem('taskData') || '[]');
+    listOfTask.push(taskObjectClass);
+    window.localStorage.setItem('taskData', JSON.stringify(listOfTask));
+    displayList();
+  }
+}
