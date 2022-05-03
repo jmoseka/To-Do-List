8918db3e6496fc513267f91bed3ac6cbfbfcc42d
jmoseka <jamylam3@gmail.com>
2022-05-03 22:03:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import './style.css';
-import TaskClass, {
-  displayList, addTask, updateCompletedDisplay, resetIndex,
-} from './modules/taskFunctions.js';
+import displayList, { updateCompletedDisplay, resetIndex } from './modules/taskFunctions.js';
+import TaskClass from './modules/operation.js'
 
 const inputTask = document.querySelector('#input-task');
-const taskListPlaceholder = document.querySelector('.task-list-placeholder');
 const clearBtn = document.querySelector('#clear-btn');
-
+// populate list when the page opens
 displayList();
 updateCompletedDisplay();
 
@@ -17,11 +14,9 @@ inputTask.addEventListener('keypress', (event) => {
     const toDoListData = JSON.parse(window.localStorage.getItem('taskData') || '[]');
     // Cancel the default action, if needed
     event.preventDefault();
-    const index = toDoListData.length + 1;
+    const index = toDoListData.length;
     const toDoClass = new TaskClass(inputTask.value.trim(), false, index);
-    addTask(toDoClass);
-    taskListPlaceholder.innerHTML = '';
-    displayList();
+    toDoClass.addTask(toDoClass);
     inputTask.value = '';
   }
 });
@@ -31,6 +26,5 @@ clearBtn.addEventListener('click', () => {
   const clearItems = toDoListData.filter((completeList) => completeList.completed === false);
   window.localStorage.setItem('bookData', JSON.stringify(clearItems));
   resetIndex(clearItems);
-  taskListPlaceholder.innerHTML = '';
   displayList();
 });
