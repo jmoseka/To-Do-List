a92b8fe9f5158a2075e152d98f4f3c3b5609621c
jmoseka <jamylam3@gmail.com>
2022-05-06 22:20:36 +0300

    Update class operation js file

--- src/modules/classOperation.js
+++ src/modules/classOperation.js
@@ -4,17 +4,6 @@ class TaskClass {
     this.completed = completed;
     this.index = index;
   }
-
-  // eslint-disable-next-line class-methods-use-this
-  addTask(taskStorage, taskClass) {
-    // const listOfTask = [];
-    taskStorage.push(taskClass);
-    return taskStorage;
-  }
 }
 
-function removeTask(obj, id) {
-  obj.splice(id, 1);
-}
-
-module.exports = { TaskClass, removeTask };
+module.exports = { TaskClass };
